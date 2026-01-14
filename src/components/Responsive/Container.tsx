3a3f9432adcbc712ce4322df92ba2bc73ff3a0ff
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-14 22:46:28 +0600

    compete footer sectionm

--- src/components/Responsive/Container.tsx
+++ src/components/Responsive/Container.tsx
@@ -5,7 +5,7 @@ interface props {
 }
 
 const Container = ({ children }: props) => {
-  return <div className="max-w-300 mx-auto px-4">{children}</div>;
+  return <div className="max-w-250 mx-auto px-4">{children}</div>;
 };
 
 export default Container;

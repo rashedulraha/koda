18d4bb1f580cd760803ca2272e293535c31f85ea
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-14 23:42:54 +0600

    adding contact us header

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,8 +1,20 @@
+import Container from "@/components/Responsive/Container";
+
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact page</h1>
-    </div>
+    <Container>
+      <div className="my-8 md:my-14 lg:my-20">
+        <div className="text-center">
+          <h1 className="text-foreground text-3xl md:text-5xl lg:text-6xl font-bold">
+            How can we help?
+          </h1>
+          <p className="text-muted-foreground text-sm md:text-xl lg:text-lg mt-2 md:mt-3 lg:mt-5">
+            Get in touch with our sales and support teams for <br /> demos,
+            onboarding support, or product questions.
+          </p>
+        </div>
+      </div>
+    </Container>
   );
 };
 

c03511dff79caff399527c5c5d30a1a2e100d2a9
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-15 00:45:48 +0600

    finished contact page

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,15 +1,16 @@
 import Container from "@/components/Responsive/Container";
 import CardComponent from "./_Shared/Card/Card";
-import Link from "next/link";
-import { MoveUpRight } from "lucide-react";
+import { CiCircleCheck } from "react-icons/ci";
+
 import Call_To_Action from "./_Shared/Call_To_Action/Call_To_Action";
+import Link from "next/link";
 
 const Contact = () => {
   return (
     <Container>
       <div className="mt-8 md:mt-14 lg:mt-20">
         <div className="text-center">
-          <h1 className="text-foreground text-3xl md:text-5xl lg:text-6xl font-bold">
+          <h1 className="text-foreground text-4xl md:text-5xl lg:text-6xl font-bold">
             How can we help?
           </h1>
           <p className="text-muted-foreground text-sm md:text-xl lg:text-lg mt-2 md:mt-3 lg:mt-5">
@@ -17,8 +18,8 @@ const Contact = () => {
             onboarding support, or product questions.
           </p>
         </div>
-        <div className="flex items-center gap-3 md:gap-5 my-5 md:my-10 lg:my-14 ">
-          <div className="flex-1">
+        <div className="flex items-center flex-col md:flex-row w-full gap-5 md:gap-8 my-5 md:my-10 lg:my-14 ">
+          <div className="flex-1 w-full">
             <CardComponent
               data={{
                 title: "Sales",
@@ -28,7 +29,7 @@ const Contact = () => {
               }}
             />
           </div>
-          <div className="flex-1">
+          <div className="flex-1 w-full">
             <CardComponent
               data={{
                 title: "Help and support",
@@ -54,33 +55,39 @@ const Contact = () => {
           <div>
             <Call_To_Action
               data={{
-                title: "Join the community",
+                title: "General communication",
                 subTitle:
-                  "More than 20,000 Linear users share questions and best practices in our Slack community.",
-                action: "Join slack",
+                  "For other queries, please get in touch with us via email.",
+                action: "hello@linear.app",
               }}
             />
           </div>
           <div>
             <Call_To_Action
               data={{
-                title: "Join the community",
+                title: "Documentation",
                 subTitle:
-                  "More than 20,000 Linear users share questions and best practices in our Slack community.",
-                action: "Join slack",
+                  "Get an overview of Linear's features, integrations, and how to use them",
+                action: "Koda docs",
               }}
             />
           </div>
           <div>
             <Call_To_Action
               data={{
-                title: "Join the community",
+                title: "Developers",
                 subTitle:
-                  "More than 20,000 Linear users share questions and best practices in our Slack community.",
-                action: "Join slack",
+                  "Learn how to use the GraphQL API and TypeScript SDK to extend Linear.",
+                action: "Koda api",
               }}
             />
           </div>
+        </div>
+
+        <div className="mt-10 flex items-center gap-1 justify-center my-5 md:my-10 lg:my-14 text-sidebar-primary hover:underline ">
+          <CiCircleCheck />
+
+          <Link href={"/all-system-operation"}>All system operation</Link>
         </div>
       </div>
     </Container>

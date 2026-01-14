93dc934445d12fc9405b1bbd2888c28869d86a9d
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-14 16:26:01 +0600

    adding navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -27,8 +27,24 @@ const Navbar = () => {
                 <NavigationMenuItem>
                   <NavigationMenuTrigger>Item One</NavigationMenuTrigger>
                   <NavigationMenuContent>
-                    <NavigationMenuLink>Link</NavigationMenuLink>
-                    <ul className="w-100">Hello</ul>
+                    <div className="grid grid-cols-2 gap-2 md:w-125 lg:w-150 lg:grid-cols-[.75fr_1fr]">
+                      <div className="bg-red-500">Hello</div>
+                      <div>Hello</div>
+                      <div>Hello</div>
+                      <div>Hello</div>
+                    </div>
+                    <div className="w-full py-3 flex items-center justify-between">
+                      <div className="flex items-center">
+                        <h2 className="font-bold text-foreground">
+                          New : Team owner
+                        </h2>
+                        <span className="text-muted-foreground ml-2">
+                          permission for team level setting
+                        </span>
+                      </div>
+
+                      <span>Changelog</span>
+                    </div>
                   </NavigationMenuContent>
                 </NavigationMenuItem>
               </NavigationMenuList>

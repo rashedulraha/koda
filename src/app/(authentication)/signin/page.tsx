12e3b4a207adf364d232cddc3490ddddf44bf545
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-20 13:24:35 +0600

    adding signup form

--- src/app/(authentication)/signin/page.tsx
+++ src/app/(authentication)/signin/page.tsx
@@ -1,9 +1,10 @@
-import { SignInForm } from "./_shared/SignInForm/SignInform";
+import SignInform from "./_shared/SignInForm/SignInform";
+
 
 const SignIn = () => {
   return (
     <div className=" py-8 md:py-10 lg:py-15 flex items-center justify-center">
-      <SignInForm />
+      <SignInform />
     </div>
   );
 };

474de815aef8682c5e321667a08f2bfff2aa622f
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-20 14:19:30 +0600

    udpate more page'

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "./globals.css"
+
 import { ThemeProvider } from "@/components/ui/theme-provider";
 
 
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -61,7 +61,7 @@ const Navbar = () => {
       </div>
 
       {/* mobile menu section */}
-      <div className="md:hidden w-full  bg-card absolute z-10 top-15">
+      <div className="md:hidden w-full  bg-card absolute z-10 top-15" data-aos="fade-down">
         <Container>
           {open && <div className="py-5">{desktopMobileMenuLink}</div>}
         </Container>

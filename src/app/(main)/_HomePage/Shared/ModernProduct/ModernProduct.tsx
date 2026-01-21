5664453f8f72843873b03a857d531217f52acc43
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-21 11:54:59 +0600

    addad card section and udpate more page"

--- src/app/(main)/_HomePage/Shared/ModernProduct/ModernProduct.tsx
+++ src/app/(main)/_HomePage/Shared/ModernProduct/ModernProduct.tsx
@@ -2,6 +2,20 @@
 import image1 from "@/modern_Image/modern1.png";
 import image2 from "@/modern_Image/modern2.png";
 import image3 from "@/modern_Image/modern3.png";
+const cards = [
+  {
+    image: image1,
+    title: "Purpose built for product development",
+  },
+  {
+    image: image2,
+    title: "Purpose built for product development",
+  },
+  {
+    image: image3,
+    title: "Purpose built for product development",
+  },
+];
 
 import { Button } from "@/components/ui/button";
 import { ChevronRight, Plus } from "lucide-react";
@@ -10,7 +24,7 @@ import Image from "next/image";
 
 const ModernProduct = () => {
   return (
-    <div className="my-15 md:my-20 lg:my-25">
+    <div className="my-15 md:my-20 lg:my-25 ">
       <div>
         {/* first section */}
         <div className="flex items-center">
@@ -34,64 +48,28 @@ const ModernProduct = () => {
         </div>
 
         {/* second section */}
-        <div className="my-5 md:my-8 lg:my-12  grid grid-cols-3 gap-3">
-          <div className="border border-muted rounded-2xl">
-            <div className="w-full">
-              {
+        <div className="my-5 md:my-8 lg:my-12 flex overflow-x-auto gap-4 pb-4 snap-x no-scrollbar">
+          {cards.map((card, index) => (
+            <div
+              key={index}
+              className="min-w-70 snap-center border rounded-2xl bg-card">
+              <div className="w-full">
                 <Image
                   className="w-full object-cover"
-                  src={image1}
-                  alt={"modern image one"}></Image>
-              }
-            </div>
-            <div className=" px-5 pb-5 flex items-center justify-between">
-              <h2 className="text-base lg:text-xl font-bold">
-                Purpose built for product development
-              </h2>
+                  src={card.image}
+                  alt="modern image"
+                />
+              </div>
 
-              <Button variant={"outline"} className="cursor-pointer">
-                <Plus />
-              </Button>
-            </div>
-          </div>
-          <div className="border border-muted rounded-2xl">
-            <div className="w-full">
-              {
-                <Image
-                  className="w-full object-cover"
-                  src={image2}
-                  alt={"modern image one"}></Image>
-              }
-            </div>
-            <div className=" px-5 pb-5 flex items-center justify-between">
-              <h2 className="text-base lg:text-xl font-bold">
-                Purpose built for product development
-              </h2>
+              <div className="px-5 pb-5 flex items-center justify-between gap-3">
+                <h2 className="text-base lg:text-xl font-bold">{card.title}</h2>
 
-              <Button variant={"outline"} className="cursor-pointer">
-                <Plus />
-              </Button>
+                <Button variant="outline" className="cursor-pointer">
+                  <Plus />
+                </Button>
+              </div>
             </div>
-          </div>
-          <div className="border border-muted rounded-2xl">
-            <div className="w-full">
-              {
-                <Image
-                  className="w-full object-cover"
-                  src={image3}
-                  alt={"modern image one"}></Image>
-              }
-            </div>
-            <div className=" px-5 pb-5 flex items-center justify-between">
-              <h2 className="text-base lg:text-xl font-bold">
-                Purpose built for product development
-              </h2>
-
-              <Button variant={"outline"} className="cursor-pointer">
-                <Plus />
-              </Button>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </div>

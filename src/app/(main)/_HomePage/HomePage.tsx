8117eec09763906de8bfd622087bf9bae6424b37
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-26 17:05:47 +0600

    aceternity bg

--- src/app/(main)/_HomePage/HomePage.tsx
+++ src/app/(main)/_HomePage/HomePage.tsx
@@ -13,10 +13,10 @@ const HomePage = () => {
   return (
     <>
       <div>
-        <Container>
-          <div className="mt-10 md:mt-20">
-            {/* first section */}
-            <div className="bg-red500">
+        <div className="">
+          {/* first section */}
+          <div className="pt-10 md:pt-20 h-screen">
+            <Container>
               <h1 className="text-2xl text-left sm:text-3xl md:text-5xl lg:text-6xl font-medium md:leading-17 text-wrap">
                 Koda is a purpose-built tool for <br /> planning and building
                 products
@@ -35,53 +35,54 @@ const HomePage = () => {
                   koda agent for slack <ChevronRight />
                 </Button>
               </div>
-            </div>
-            {/* second section */}
-            <div className="mt-30">
-              <div className="text-center space-y-2">
-                <h3 className="text-base md:text-lg lg:text-xl font-semibold">
-                  Powering the world’s best product teams.
-                </h3>
-                <h3 className="text-base md:text-lg lg:text-xl font-semibold text-muted-foreground">
-                  From next-gen startups to established enterprises.
-                </h3>
-              </div>
+            </Container>
+          </div>
 
-              {/* hover and customer section  */}
-              <div className="my-5 md:my-10 relative group ">
-                <div>
-                  <div className="hidden lg:grid grid-cols-4 gap-11 items-center justify-center place-content-center text-center transition-all decoration-800 group group-hover:blur-sm">
-                    {/*  customer data  */}
-                    {Customer_Data.map((customer) => (
-                      <>
-                        <h2 key={customer.id} className="text-2xl font-bold">
-                          {customer.name}
-                        </h2>
-                      </>
-                    ))}
-                  </div>
-                  <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  group opacity-0 group-hover:opacity-100">
-                    <Link href={"/meet-our-customer"}>
-                      <Button
-                        variant={"outline"}
-                        className="rounded-full cursor-pointer">
-                        Meet our customer <ChevronRight />
-                      </Button>
-                    </Link>
-                  </div>
-                  <div className="lg:hidden">
-                    <Customer_Marquee />
-                  </div>
+          {/* second section */}
+          <div className="mt-30">
+            <div className="text-center space-y-2">
+              <h3 className="text-base md:text-lg lg:text-xl font-semibold">
+                Powering the world’s best product teams.
+              </h3>
+              <h3 className="text-base md:text-lg lg:text-xl font-semibold text-muted-foreground">
+                From next-gen startups to established enterprises.
+              </h3>
+            </div>
+
+            {/* hover and customer section  */}
+            <div className="my-5 md:my-10 relative group ">
+              <div>
+                <div className="hidden lg:grid grid-cols-4 gap-11 items-center justify-center place-content-center text-center transition-all decoration-800 group group-hover:blur-sm">
+                  {/*  customer data  */}
+                  {Customer_Data.map((customer) => (
+                    <>
+                      <h2 key={customer.id} className="text-2xl font-bold">
+                        {customer.name}
+                      </h2>
+                    </>
+                  ))}
+                </div>
+                <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  group opacity-0 group-hover:opacity-100">
+                  <Link href={"/meet-our-customer"}>
+                    <Button
+                      variant={"outline"}
+                      className="rounded-full cursor-pointer">
+                      Meet our customer <ChevronRight />
+                    </Button>
+                  </Link>
+                </div>
+                <div className="lg:hidden">
+                  <Customer_Marquee />
                 </div>
               </div>
             </div>
-
-            {/* third section  */}
-            <div>
-              <ModernProduct />
-            </div>
           </div>
-        </Container>
+
+          {/* third section  */}
+          <div>
+            <ModernProduct />
+          </div>
+        </div>
       </div>
       {/* fourth section */}
       <div className="bg-linear-to-b from-card to-background py-8 md:my-12 lg:py-14">

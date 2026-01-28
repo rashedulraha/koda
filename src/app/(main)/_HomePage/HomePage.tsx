26544f02a136b2ba0c06136b0085881a11c9d77c
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-01-28 18:56:32 +0600

    udpate more page

--- src/app/(main)/_HomePage/HomePage.tsx
+++ src/app/(main)/_HomePage/HomePage.tsx
@@ -6,7 +6,6 @@ import Product_direction from "./Shared/Product_direction/Product_direction";
 import { BackgroundBeamsDemo } from "./Shared/BackgroundBeamsDemo/BackgroundBeamsDemo";
 import Second from "./Shared/Second/Second";
 import { WorldMapDemo } from "./Shared/WorldMap/WorldMapDemo";
-import FeaturesSection from "./Shared/FeaturesSection/FeaturesSection";
 
 const HomePage = () => {
   return (
@@ -33,10 +32,6 @@ const HomePage = () => {
 
       <div className="bg-linear-to-b from-card to-background py-8 md:my-12 lg:py-14">
         <Product_direction />
-      </div>
-
-      <div>
-        <FeaturesSection />
       </div>
 
       <div>

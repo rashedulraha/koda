7e7b6f753ec66336139bb793c8f33d7fc4f35614
Rashedul Islam <rashedulraha.bd@gmail.com>
2026-03-08 21:15:00 +0600

    adding navbar  and highlight navlink

--- src/app/(main)/(new root layout)/layout.tsx
+++ src/app/(main)/(new root layout)/layout.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Container from "@/components/Responsive/Container";
 import { Button } from "@/components/ui/button";
 import { ButtonGroup } from "@/components/ui/button-group";
@@ -5,25 +7,44 @@ import { Field, FieldLabel } from "@/components/ui/field";
 import { Input } from "@/components/ui/input";
 import { Wifi } from "lucide-react";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
-const layout = ({ children }: { children: React.ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
+  const pathname = usePathname();
+
+  const navLinks = [
+    { name: "All", path: "/new" },
+    { name: "Changelog", path: "/changelog" },
+    { name: "Community", path: "/community" },
+    { name: "News", path: "/news" },
+    { name: "Craft", path: "/craft" },
+    { name: "Ai", path: "/ai" },
+    { name: "Practices", path: "/practices" },
+    { name: "Press", path: "/press" },
+  ];
+
   return (
     <Container>
       <div className="mt-8 md:mt-14 lg:mt-20">
-        <h1 className="capitalize font-bold text-lg md:text-2xl"> New</h1>
+        <h1 className="capitalize font-bold text-lg md:text-2xl">New</h1>
       </div>
 
-      <div className="mt-5 md:mt-8 lg:mt-10 flex items-end justify-between ">
-        <div className="font-normal text-foreground text-sm md:text-base flex gap-5 capitalize">
-          <Link href={"/new"}>All</Link>
-          <Link href={"/changelog"}>Changelog</Link>
-          <Link href={"/community"}>Community</Link>
-          <Link href={"/news"}>News</Link>
-          <Link href={"/craft"}>Craft</Link>
-          <Link href={"/ai"}>Ai</Link>
-          <Link href={"/practices"}>Practices</Link>
-          <Link href={"/press"}>Press</Link>
+      <div className="mt-5 md:mt-8 lg:mt-10 flex items-end justify-between">
+        <div className="font-normal text-sm md:text-base flex gap-5 capitalize text-muted-foreground">
+          {navLinks.map((link) => (
+            <Link
+              key={link.path}
+              href={link.path}
+              className={`${
+                pathname === link.path
+                  ? "text-foreground font-semibold "
+                  : "hover:text-foreground"
+              }`}>
+              {link.name}
+            </Link>
+          ))}
         </div>
+
         <div>
           <Field>
             <FieldLabel htmlFor="input-button-group">Search</FieldLabel>
@@ -43,4 +64,4 @@ const layout = ({ children }: { children: React.ReactNode }) => {
   );
 };
 
-export default layout;
+export default Layout;
